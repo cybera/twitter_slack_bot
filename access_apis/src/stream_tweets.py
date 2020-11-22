468300c7a63a0b97a4adbeedef7394d2e31f587a
Vishal <vishal3@ualberta.ca>
2020-11-22 16:35:40 -0700

    Extend the twitter api requests for multiple users

--- access_apis/src/stream_tweets.py
+++ access_apis/src/stream_tweets.py
@@ -95,14 +95,27 @@ def real_time_tweets(query, first_time=None, last_tweet_id=None):
 
 
 if __name__ == "__main__":
-    
-    query_file = open("./sample_queries.txt", "r")
 
-    query =  query_file.read()
+    query_file_path = "./sample_queries.txt"
 
-    last_id = real_time_tweets(query, first_time=True)
-    # intialize slack
-    while last_id:
-        last_id = real_time_tweets(query, last_tweet_id=last_id)
+    with open(query_file_path, "r") as f:
+        query_content = f.readlines()
+
+    query_list = [x.strip() for x in query_content]
+    list_last_ids = np.array([])
+
+    for query in query_list:
+        print("Running", query)
+        last_id = real_time_tweets(query, first_time=True)
+        list_last_ids = np.append(list_last_ids, last_id)
+
+    while True:
+        ind_flag = 0
+        for query in query_list:
+            last_id = real_time_tweets(query, last_tweet_id=list_last_ids[ind_flag])
+            if last_id != list_last_ids[ind_flag]:
+                list_last_ids[ind_flag] = last_id
+            ind_flag += 1
+
         print("Last refreshed for new tweets at", datetime.datetime.now())
         time.sleep(20)
