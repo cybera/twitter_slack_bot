2caf9f572dd899a69c333f0799cd57fe709a63a2
Vishal <vishal3@ualberta.ca>
2020-11-22 15:03:02 -0700

    Since-id in loop for streaming realtime tweets

--- access_apis/src/slack_bot.py
+++ access_apis/src/slack_bot.py
@@ -4,25 +4,23 @@ import logging
 from slack.web.client import WebClient
 from slack.errors import SlackApiError
 
+
 def auth():
     return os.environ.get("BEARER_TOKEN_SLACK")
 
+
 def post_message_to_slack(slack_client, msg):
 
-  try:
-    slack_client.chat_postMessage(
-      channel='#testing',
-      text=msg
-    )
-  except SlackApiError as e:
-    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
-    logging.error(e.response)
+    try:
+        slack_client.chat_postMessage(channel="#testing", text=msg)
+    except SlackApiError as e:
+        logging.error("Request to Slack API Failed: {}.".format(e.response.status_code))
+        logging.error(e.response)
 
 
 def slackbot(msg):
     slack_bot_token = auth()
     slack_client = WebClient(slack_bot_token)
     # # For testing
-    #msg = "Good Afternoon! Testing from Python script"
+    # msg = "Good Afternoon! Testing from Python script"
     post_message_to_slack(slack_client, msg)
-
--- access_apis/src/stream_tweets.py
+++ access_apis/src/stream_tweets.py
@@ -4,7 +4,7 @@ import requests
 import os
 import json
 import time
-import schedule
+import datetime
 import pandas as pd
 import numpy as np
 
@@ -18,7 +18,7 @@ def auth():
     return os.environ.get("BEARER_TOKEN_TWITTER")
 
 
-def create_url(query, last_tweet = None):
+def create_url(query, last_tweet=None):
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -26,12 +26,12 @@ def create_url(query, last_tweet = None):
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    if (query):
+    if query:
         tweet_fields = "tweet.fields=created_at"
         expansion_field = "expansions=author_id"
-        if (last_tweet):
+        if last_tweet:
             since_id = "since_id=" + str(last_tweet)
-            print("Accessing Last Tweet ",since_id)
+            print("Accessing Last Tweet ", since_id)
             url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
                 query, tweet_fields, expansion_field, since_id
             )
@@ -57,39 +57,48 @@ def connect_to_endpoint(url, headers):
     return response.json()
 
 
-def main():
-    # Access the bearer_token 
+def real_time_tweets(first_time=None, last_tweet_id=None):
+    # Access the bearer_token
     bearer_token = auth()
     # Send in the query string and last tweet
     query = "from:CMOH_Alberta -is:retweet"
-    since_id = 1329873969940316160
 
-    url = create_url(query, last_tweet = since_id)
-    headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
-    if (json_response):
-        #print(json.dumps(json_response, indent=4, sort_keys=True))
-        df_tweet = pd.json_normalize(json_response['data'])
-        df_name = pd.json_normalize(json_response['includes']["users"])
-        #print(df_tweet['auth'].columns)
-        #print(np.arange(df_tweet.shape[0]))
-        
-        for ind in np.arange(df_tweet.shape[0]):
-            #name = df_name["name"].value
-            #username = "(" + df_name["username"] 
-            msg= "*" + df_name.loc[0,"name"] + "*" + " (" + ":medical_symbol:"  + df_name.loc[0,"username"]  + ") \n "+ df_tweet.loc[ind,'text']
-            #msg= df_tweet.loc[ind,'text']
-            slackbot(msg)
-        #msg="Tweet from CMOH_Alberta \n "+ df_tweet.loc[0,'text']
-        #print(df_tweet.loc[0,'text'])
-        #slackbot(msg)
-        #print(json.dumps(json_response["data"], indent=4, sort_keys=True))
-    else:
-        print("No Tweets exist")
+    if first_time:
+        url = create_url(query)
+        headers = create_headers(bearer_token)
+        json_response = connect_to_endpoint(url, headers)
+        if json_response:
+            df_week_tweets = pd.json_normalize(json_response["data"])
+            return df_week_tweets.loc[3, "id"]
+
+    if last_tweet_id:
+        since_id = last_tweet_id
+        url = create_url(query, last_tweet=since_id)
+        headers = create_headers(bearer_token)
+        json_response = connect_to_endpoint(url, headers)
+        tweet_count = json_response["meta"]["result_count"]
+        if tweet_count != 0:
+            # print(json.dumps(json_response, indent=4, sort_keys=True))
+            df_tweet = pd.json_normalize(json_response["data"])
+            df_name = pd.json_normalize(json_response["includes"]["users"])
+            for ind in np.arange(df_tweet.shape[0]):
+                name = "*" + df_name.loc[0, "name"] + "*"
+                username = "(" + df_name.loc[0, "username"] + ")  \n"
+                tweet_content = df_tweet.loc[ind, "text"]
+                msg = name + username + tweet_content
+                slackbot(msg)
+            print("Latest tweets are sent in slack messages.")
+            return df_tweet.loc[0, "id"]
+
+        else:
+            print("No new tweets exist.")
+            return since_id
+
 
 if __name__ == "__main__":
-    schedule.every(5).seconds.do(main)
-
-    while True:                                
-        schedule.run_pending()
-        time.sleep(1)
+    last_id = real_time_tweets(first_time=True)
+    # intialize slack
+    while last_id:
+        last_id = real_time_tweets(last_tweet_id=last_id)
+        print("Last refreshed for new tweets at", datetime.datetime.now())
+        time.sleep(20)
