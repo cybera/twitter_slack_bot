52f8910f38ea3ceb617d94af373695ef8714e6cf
Vishal <vishal3@ualberta.ca>
2020-12-02 11:41:32 -0700

    Create a new flag for the first time reference

--- bot_files/src/run_twitter_slack_bot.py
+++ bot_files/src/run_twitter_slack_bot.py
@@ -9,19 +9,22 @@ from stream_tweets_to_slack import real_time_tweets
 if __name__ == "__main__":
 
     query_file_path = "./sample_queries.txt"
-
-    with open(query_file_path, "r") as f:
-        query_content = f.readlines()
-
-    query_list = [x.strip() for x in query_content]
-    list_last_ids = np.array([])
-
-    for query in query_list:
-        print("Running", query)
-        last_id = real_time_tweets(query, first_time=True)
-        list_last_ids = np.append(list_last_ids, last_id)
-
+    
+    init_first_time = True 
     while True:
+
+        if init_first_time:
+            init_first_time = 0
+            with open(query_file_path, "r") as f:
+                query_content = f.readlines()
+            query_list = [x.strip() for x in query_content]
+        
+            list_last_ids = np.array([])
+            print("Initializing all queries for the first time")
+            for query in query_list:
+                last_id = real_time_tweets(query, first_time=True)
+                list_last_ids = np.append(list_last_ids, last_id)
+
         ind_flag = 0
         for query in query_list:
             last_id = real_time_tweets(query, last_tweet_id=list_last_ids[ind_flag])
