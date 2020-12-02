ab510840be1b08a81ba18c06e808c9365178d052
Vishal <vishal3@ualberta.ca>
2020-12-02 13:28:15 -0700

    make timestamp as the user input variable

--- bot_files/src/slack_bot.py
+++ bot_files/src/slack_bot.py
@@ -18,16 +18,16 @@ def post_message_to_slack(slack_client, msg, attachments = None):
         logging.error(e.response)
 
 
-def retrieve_messages_from_slack(slack_client):
+def retrieve_messages_from_slack(slack_client, ts_old):
 
     try:
-        return slack_client.conversations_history(channel=os.environ.get("SLACK_CHANNEL_ID"), oldest = 1606772429.008200)
+        return slack_client.conversations_history(channel=os.environ.get("SLACK_CHANNEL_ID"), oldest = ts_old)
     except SlackApiError as e:
         logging.error("Request to Slack API Failed: {}.".format(e.response.status_code))
         logging.error(e.response)
 
 
-def slackbot(action_string,msg = None, attachments = None):
+def slackbot(action_string,msg = None, attachments = None, time_stamp = None):
     slack_bot_token = auth_slack()
     slack_client = WebClient(slack_bot_token)
     # # For testing
@@ -36,6 +36,6 @@ def slackbot(action_string,msg = None, attachments = None):
     if action_string == "post_message":
         post_message_to_slack(slack_client, msg, attachments)
     if action_string == "retrieve_message":
-        response = retrieve_messages_from_slack(slack_client)
+        response = retrieve_messages_from_slack(slack_client, time_stamp)
         return response
         #print(response)
