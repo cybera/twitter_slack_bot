896a59d3851fc98be5849d8dfa6fae427d63041a
vishalbalashankar <vishal3@ualberta.ca>
2021-03-19 21:44:10 +0000

    Display tweet url and the content

--- bot_files/src/stream_tweets_to_slack.py
+++ bot_files/src/stream_tweets_to_slack.py
@@ -52,22 +52,33 @@ def real_time_tweets(query, first_time=None, last_tweet_id=None):
             username = response_dict_recent_search["includes"]["users"][0]["username"]
 
             for ind in range(len(response_dict_recent_search["data"])):
-                tweet_content = response_dict_recent_search["data"][ind]["text"]
-                if ind == 0:
+                status_id = response_dict_recent_search["data"][ind]["id"]
+                try: 
+                    response = response_dict_recent_search["data"][ind]["referenced_tweets"][0]['type']
                     msg = (
-                        "*" + name + "*" + " | _" + username + "_ | \n" + tweet_content
+                        "*" + name + "*" + " | _" + username + "_ |" + " -" + response + "\n"
                     )
                     slackbot(msg)
-                else:
-                    msg = tweet_content
-                    if ind == (len(response_dict_recent_search["data"]) - 1):
-                        slackbot(
-                            msg,
-                            attachments=[{"blocks": [{"type": "divider"}]}],
-                        )
-                    else:
-                        slackbot(msg)
-
+                    msg = "https://twitter.com/" +  username + "/status/" + status_id
+                    slackbot(msg)
+                except:
+                    msg = "https://twitter.com/" +  username + "/status/" + status_id
+                    slackbot(msg)
+#                tweet_content = response_dict_recent_search["data"][ind]["text"]
+#                if ind == 0:
+#                    msg = (
+#                        "*" + name + "*" + " | _" + username + "_ | \n" + tweet_content
+#                    )
+#                    slackbot(msg)
+#                else:
+#                    msg = tweet_content
+#                    if ind == (len(response_dict_recent_search["data"]) - 1):
+#                        slackbot(
+#                            msg,
+#                            attachments=[{"blocks": [{"type": "divider"}]}],
+#                        )
+#                    else:
+#                        slackbot(msg)
             print("Latest tweets are sent in slack messages.")
             return response_dict_recent_search["meta"]["newest_id"]
 
